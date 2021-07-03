9b9830f81c4ac360ba14a95be35ad6f3bd4d6f9b
Cristime <thecristime@outlook.com>
2021-07-03 21:36:38 +0800

    Finished work in 2021/7/3.

--- MyBFInterpreter/MyBFInterpreter.cpp
+++ MyBFInterpreter/MyBFInterpreter.cpp
@@ -1,12 +1,52 @@
-﻿// MyBFInterpreter.cpp: 定义应用程序的入口点。
-//
+﻿#include "MyBFInterpreter.h"
 
-#include "MyBFInterpreter.h"
+static char * paper;
 
-using namespace std;
-
-int main()
+ExecStatus Work(const char * ProgramCode, const bool isFirst, const bool isFindRightBracket)
 {
-	cout << "Hello CMake。" << endl;
-	return 0;
+	if (isFirst) Init();
+	int len = strlen(ProgramCode);
+	if (isFindRightBracket) {
+		std::stack<int> st; st.push(-1);
+		int i;
+		for (i = 0; !st.empty() && i < len; i++) {
+			const char & ch = ProgramCode[i];
+			if (ch == '[') st.push(i);
+			if (ch == ']') st.pop();
+		}
+		return ExecStatus(true, "No Info", i + 1);
+	}
+	for (int i = 0; i < len; i++) {
+		switch (ProgramCode[i]) {
+		case '+':
+			bool tmp = AddVal();
+			if (!tmp) return ExecStatus(false, "Not a avaliable ASCII character", -1);
+			break;
+		case '-':
+			bool tmp = SubVal();
+			if (!tmp) return ExecStatus(false, "Not a avaliable ASCII character", -1);
+			break;
+		case '>':
+			bool tmp = NextChar();
+			if (!tmp) return ExecStatus(false, "Paper overflow", -1);
+			break;
+		case '<':
+			bool tmp = PrevChar();
+			if (!tmp) return ExecStatus(false, "Paper overflow", -1);
+			break;
+		case '[':
+			if (Check()) { i = Work(ProgramCode + i + 1, false, false).place; break; }
+			i = Work(ProgramCode + i + 1, false, true).place; break;
+		case ']':
+			if (!Check()) {
+				if (isFirst) return ExecStatus(false, "Mismatched brackets", -1);
+				i = 0; break;
+			}
+			if (!isFirst) return ExecStatus(true, "No Info", i + 1);
+			break;
+		default:
+			return ExecStatus(false, "Undefined Character", -1);
+		}
+	}
+	return ExecStatus(true, "No Info", len);
 }
--- MyBFInterpreter/MyBFInterpreter.h
+++ MyBFInterpreter/MyBFInterpreter.h
@@ -3,6 +3,27 @@
 
 #pragma once
 
-#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <stack>
+
+const int MAXLENGTH = 1e5 + 10;
+
+struct ExecStatus {
+	ExecStatus(bool st, char * in, int pl) { status = st; info = in; place = pl; }
+	ExecStatus(const ExecStatus & old) { this->status = old.status; this->info = old.info; this->place = old.place; }
+	bool status;
+	char * info;
+	int place;
+};
+
+
+void Init();	// TODO
+ExecStatus Work(const char *, bool, bool);
+inline bool AddVal();	// TODO
+inline bool SubVal();	// TODO
+inline bool NextChar();	// TODO
+inline bool PrevChar();	// TODO
+inline bool Check();	// TODO
 
 // TODO: 在此处引用程序需要的其他标头。
